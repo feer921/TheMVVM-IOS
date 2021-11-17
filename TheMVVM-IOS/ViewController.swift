46cefa51e435e4eb7db4befdadbc5ab6fcdc5f85
lifei <lifei@laiqutech.com>
2021-11-17 11:05:27 +0800

    Initial Commit

---
+++ TheMVVM-IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMVVM-IOS
+//
+//  Created by LQ0051 on 2021/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
